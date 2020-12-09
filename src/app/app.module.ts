0e3bb3432f4e67df14da57ef5f348f07d3106499
Gian Di Serafino <gian@searchkings.ca>
2020-12-09 17:34:23 -0500

    Updated to latest angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
-import { PongGameComponent } from "./pong/pong.component";
+import { AppComponent } from './app.component';
+import { PongGameComponent } from './pong/pong.component';
 
 @NgModule({
   declarations: [AppComponent, PongGameComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
